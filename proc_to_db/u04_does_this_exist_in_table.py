b35a6adfb86755a89e72257fd15e8332ad8bb275
robertoignacio <rvs.git@gmail.com>
2024-02-16 16:46:51 -0300

    rename of script 02

--- proc_to_db/u04_does_this_exist_in_table.py
+++ proc_to_db/u04_does_this_exist_in_table.py
@@ -1,2 +1,23 @@
+import sqlite3
+
+# Description: This file is used to check if a domain name exists in the table.
+
+import control_vars as cv # From control_vars.py file, values to be set by a Control Panel.
+
+tld = cv.tld
+# tld_s is for naming the files and tables
+tld_s = str(tld.lstrip('.'))
+
+# ---------------------------------
+# sqlite db path
+db_path = f'../db_store/{tld_s}_tld_domain_names.db'
+# db_connection = sqlite3.connect(f'../db_store/{tld_s}_tld_domain_names.db')
+db_connection = sqlite3.connect(db_path)
+
+# Cursor, to execute SQL commands
+cursor = db_connection.cursor()
+
+# ---------------------------------
+
 
 
