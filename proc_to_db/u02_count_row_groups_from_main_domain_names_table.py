05e185162406755f39ddb86e0f3f09a611fbfa63
robertoignacio <rvs.git@gmail.com>
2024-02-16 16:37:05 -0300

    updated script u02 variable imports

--- proc_to_db/u02_count_row_groups_from_main_domain_names_table.py
+++ proc_to_db/u02_count_row_groups_from_main_domain_names_table.py
@@ -2,13 +2,12 @@ import sqlite3
 
 # Description : This script prints all domain name lengths within the domain_names_table, each with the number of registered domain names of that length.
 
-# Variables for the control flow of the program
-# control_vars.py is at the same directory (level) as this file
-import control_vars as cv
+import control_vars as cv # From control_vars.py file, values to be set by a Control Panel.
 
 tld = cv.tld
 tld_s = str(tld.lstrip('.'))
 
+# ------------------------------------------------
 # sqlite db path
 db_path = f'../db_store/{tld_s}_tld_domain_names.db'
 # db_connection = sqlite3.connect(f'../db_store/{tld_s}_tld_domain_names.db')
