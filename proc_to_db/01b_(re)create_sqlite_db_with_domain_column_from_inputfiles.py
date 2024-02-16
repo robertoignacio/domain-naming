4cdd1a55f5429057b0f1c37dc181d7cf63abbcfd
robertoignacio <rvs.git@gmail.com>
2024-02-16 07:35:58 -0300

    simple rename with .gitignore update

--- proc_to_db/01b_(re)create_sqlite_db_with_domain_column_from_inputfiles.py
+++ proc_to_db/01b_(re)create_sqlite_db_with_domain_column_from_inputfiles.py
@@ -2,17 +2,20 @@ import sqlite3
 from tqdm import tqdm # progress bar
 import uuid # generates a random UUID
 
-# Description: This script reads a file line by line and inserts the data into a sqlite database table.
+# Description: This script creates a sqlite database file from a file that contains domain names.
 
-# Variables for the control flow of the program
-# control_vars.py is at the same directory (level) as this file
-import control_vars as cv
+tld = str('.dev')
+# tld_s is for naming the files and tables
+tld_s = str(tld.lstrip('.'))
 
-tld = cv.tld
-tld_s = cv.tld_s
+# Dump file to be read, that contains the domain names
+input_file = open(f'../inputfiles/icann_tld_{tld_s}_global_list_latest.txt', 'r')
 
-# Create and/or connect to sqlite file db
-db_connection = cv.db_connection
+# Create or connect to sqlite file db
+# sqlite db path
+db_path = f'../db_store/{tld_s}_tld_domain_names.db'
+# db_connection = sqlite3.connect(f'../db_store/{tld_s}_tld_domain_names.db')
+db_connection = sqlite3.connect(db_path)
 
 # Cursor, to execute SQL commands
 cursor = db_connection.cursor()
@@ -30,12 +33,10 @@ cursor = db_connection.cursor()
 # Create table
 cursor.execute(f'''
     CREATE TABLE IF NOT EXISTS "domain_names_table"
-    (id INTEGER PRIMARY KEY "domain_name" TEXT) STRICT
+    (id TEXT PRIMARY KEY "domain_name" TEXT) STRICT
 ''')
 
 # [  domain_names_table  ]
 # [  id  ] [ domain_name ]
 
-# read the file line by line
-# input_file = open(f'../inputfiles/dummy.txt', 'r')
-input_file = open(f'../inputfiles/icann_tld_dev_global_list_2024_02.txt', 'r')
+# Read the file line by line
